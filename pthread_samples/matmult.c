0d507d027e00a643f846030c150c5673d3769769
Tim Waterman <watermantium@gmail.com>
2015-10-24 12:57:02 -0400

    Pthread skeleton working

--- pthread_samples/matmult.c
+++ pthread_samples/matmult.c
@@ -4,46 +4,64 @@
 
 const int SIZE = 10000;
 
+struct rangeInfo {
+
+    int begin;
+    int end;
+    int stepSize;
+
+};
+
 int a[SIZE];
 int c[SIZE];
 
-void *addNtoAll(void *index) { //each thread operates on a single sector of the matrices
+void *addNtoAll(void *info) { //each thread operates on a single sector of the matrices
 
-    int count = (int *)index;
+    struct rangeInfo *myInfo = (struct rangeInfo *)info;
 
-    int i;
-    c[count] = a[count] + 1;
+    printf("Hello from thread with range %d to %d\n", myInfo->begin, myInfo->end);
 
-    }
+    return (void *)0;
 }
 
 
 int main(int argc, char **argv) {
 
+    int numThreads = 4;
 
     //it's like deadpool, but with threads
-    pthread_t *threadpool = malloc(SIZE * sizeof(pthread_t));
+    pthread_t *threadpool = malloc(numThreads * sizeof(pthread_t));
 
     if (threadpool == NULL) {
         perror("Malloc failed\n");
         exit(1);
     }
 
-    for(i = 0; i < SIZE; i++) {
+    struct rangeInfo *info = malloc(sizeof(struct rangeInfo) * numThreads);
+
+    int i;
+    int thread = 0;
+
+    for(i = 0; i < SIZE; i += SIZE/numThreads) {
+
+        info[thread].begin = i;
+        info[thread].end = i + SIZE/numThreads - 1;
+        info[thread].stepSize = 1;
         
-        int e = pthread_create(&threadpool[i], NULL, multiply, (void *)i);
+        int e = pthread_create(&threadpool[thread], NULL, addNtoAll, &info[thread]);
         if (e != 0) {
             perror("Cannot create thread!\n");
-            free(threadpool);
+            free(threadpool); //error, free the threadpool
             exit(1);
         }
+
+        thread++;
     }
 
     //loop and wait for all the threads to finish
-    for(i = 0; i < SIZE * SIZE; i++) {
-        pthread_join(&threadpool[i], NULL);
+    for(i = 0; i < numThreads; i++) {
+        pthread_join(threadpool[i], NULL);
     }
-
 
     return 0;
 }
