3baf3d29959ee88b8bb4afa3542c351947e17965
Daniel Cole <takeitfromthedan@gmail.com>
2015-11-09 10:54:22 -0500

    stch c headers and func defs added

--- runtime/stch_headers.h
+++ runtime/stch_headers.h
@@ -31,23 +31,31 @@
 
 //hold local variables to pass from the stitch loop into a thread
 //need to figure this out...
-struct stchLocalVars{
+struct stch_LocalVars{
     
-    void *vars;
-    int  n;
+    void 		  *vars;
+    unsigned int  n;
 
 };
 
 //range info passed into the thread
-struct rangeInfo {
+struct stch_rangeInfo{
 
-    int begin;
-    int end;
-    int stepSize;
-    int cols;
-    struct stchLocalVars *locals;
+    int 	begin;
+    int 	end;
+    int 	stepSize;
+    int 	cols;
+    struct 	stchLocalVars *locals;
 
 };
+
+//array wrapper
+typedef struct stch_array{
+
+	void 			*data;
+	unsigned int 	length;
+
+} stch_array;
 
 /*
 ************************
@@ -55,10 +63,22 @@ struct rangeInfo {
 ************************
 */
 
-void stch_print(const char* s);
-void stch_error(const char* e);
-void stch_read(const int fd, void* dest);
-void stch_write(const int fd, void* source);
+//open()
 int  stch_open(const char* source);
+//write()
+int  stch_write(const int fd, stch_array* source);
+//read()
+int  stch_read(const int fd, stch_array* dest);
+//print()
+void stch_print(const char* s);
+//error()
+void stch_error(const char* e);
+//lengthof()
+int  stch_length(const stch_array* a);
+//exit()
+void stch_exit(int e);
+//cut()
+void stch_cut(int e);
+
 
 #endif
