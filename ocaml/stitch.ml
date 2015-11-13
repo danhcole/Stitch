71d238926bffe47ecf9d150e01ac641de54fad5c
Tim Waterman <watermantium@gmail.com>
2015-11-12 21:55:39 -0500

    Stitch includes headers

--- ocaml/stitch.ml
+++ ocaml/stitch.ml
@@ -1,3 +1,6 @@
+(*------------------------------------------------*)
+(* Parse and print the program                    *)
+(*------------------------------------------------*)
 
 let filename = Sys.argv.(1) ^ ".c" in
 
@@ -6,5 +9,6 @@ let in_channel = open_in Sys.argv.(1) in
 let lexbuf = Lexing.from_channel in_channel in
 
 let program = Stch_parser.program Stch_scanner.token lexbuf in
-let listing = Stch_ast.string_of_program program
-		in Printf.fprintf (open_out filename) "%s" listing
+let listing = Stch_ast.string_of_program program in
+let headers = "#include \"stch_headers.h\"\n\n" ^ listing 
+		in Printf.fprintf (open_out filename) "%s" headers
