000a738da56f1e36a2b1fa686672388d86370d9f
Tim Waterman <watermantium@gmail.com>
2015-11-12 23:42:58 -0500

    Colorized output of ptest suite

--- testing/stch_ptestSuite.sh
+++ testing/stch_ptestSuite.sh
@@ -2,10 +2,29 @@
 
 #Stitch Lang Regression Test Suite for Parser
 
+COL='\033[0;34m'
+SUCC='\033[1;32m'
+FAIL='\033[0;31m'
+NC='\033[0m'
+
 STITCH="./../ocaml/stitch"
 DECTESTS="./_ptests/dec*"
 FUNCTESTS="./_ptests/fun*"
 LOOPTESTS="./_ptests/loop*"
+
+function echoResult {
+
+	if [ $1 -eq 0 ]; then
+		echo "${SUCC}TEST SUCCESSFUL!${NC}"
+	else
+		echo "${FAIL}TEST FAILED!${NC}"
+	fi
+}
+
+function printTest {
+
+	echo $COL$(head -n 1 $1) $NC
+}
 
 echo "Making the compiler..."
 cd ../ocaml
@@ -21,8 +40,9 @@ echo "Declaration Tests"
 for test in $DECTESTS
 do 
 	echo "Starting test $test"
-	echo $(head -n 1 $test)
-	$STITCH $test 
+	printTest $test
+	$STITCH $test
+	echoResult $? 
 	echo "\n"
 done
 
@@ -31,16 +51,18 @@ echo "Function Tests"
 for test in $FUNCTESTS
 do 
 	echo "Starting test $test"
-	echo $(head -n 1 $test)
+	printTest $test
 	$STITCH $test
+	echoResult $?
 	echo "\n"
 done
 
 for test in $LOOPTESTS
 do 
 	echo "Starting test $test"
-	echo $(head -n 1 $test)
+	printTest $test
 	$STITCH $test
+	echoResult $?
 	echo "\n"
 done
 
