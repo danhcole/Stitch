479c8701c4df07f1f37653b9d5013ef21b2c2fa5
Tim Waterman <watermantium@gmail.com>
2015-12-03 20:15:58 -0500

    Started the C code generation file

---
+++ ocaml/c_generator.ml
@@ -0,0 +1,64 @@
+open Stch_ast
+open Stch_cast
+
+let string_of_c_dataType = function
+  Tint -> "int"
+  | Tfloat -> "float"
+  | Tchar -> "char"
+  | Tvoid -> "void"
+
+let rec string_of_c_expr = function
+    C_Int(l) -> string_of_int l
+  | C_Float(l) -> string_of_float l
+  | C_Char(l) ->  "\'" ^ String.make 1 l ^ "\'"
+  | C_Id(s) -> s
+  | C_String(s) -> "\"" ^ s ^ "\""
+  | C_Binop(e1, o, e2) ->
+      string_of_c_expr e1 ^ " " ^
+      (match o with
+	Add -> "+" | Subtract -> "-" | Times -> "*" | Divide -> "/"
+      | Equal -> "==" | Ne -> "!="
+      | Lt -> "<" | Le -> "<=" | Gt -> ">" | Ge -> ">="
+      | Or -> "||" | And -> "&&" | Mod -> "%" ) ^ " " ^
+      string_of_c_expr e2
+  | C_Negate(e) -> "!" ^ string_of_c_expr e
+  | C_Call(f, el) -> (match f with "print" -> "printf" | _ -> f) ^ "(" ^ String.concat ", " (List.map string_of_c_expr el) ^ ")"
+  | C_Assign2(i, e) -> i ^ " = " ^ string_of_c_expr e ^ ";\n"
+  (*  Array_Item_Assign(id, ind, e) -> id ^ "[" ^ string_of_int ind ^"] = " ^ string_of_c_expr e ^ ";\n" *)
+  (* | C_Access(f, s) -> f ^ "." ^ s  *)
+  | C_Noexpr -> ""
+
+let string_of_c_vdecl vdecl = string_of_c_dataType vdecl.vdecl_type ^ " " ^ vdecl.vdecl_name (* " " ^ vdecl.array_size ^ *)
+
+(* let string_of_c_arraydecl arraydecl = string_of_c_dataType arraydecl.arraydecl_type ^ " " ^ arraydecl.arraydecl_name ^ "[" ^
+    string_of_c_expr arraydecl.arraydecl_size ^ "]" *)
+
+let rec string_of_c_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_c_stmt stmts) ^ "}\n"
+  | C_Expr(c_expr) -> string_of_c_expr c_expr ^ ";\n";
+  | C_Vdecl(v) -> string_of_c_dataType v.vdecl_type ^ " " ^ v.vdecl_name ^ ";\n";
+  | C_Return(c_expr) -> "return " ^ string_of_c_expr c_expr ^ ";\n";
+  | C_If(e, s, Block([])) -> "if (" ^ string_of_c_expr e ^ ")\n" ^ string_of_c_stmt s
+  | C_If(e, s1, s2) ->  "if (" ^ string_of_c_expr e ^ ")\n" ^
+      string_of_c_stmt s1 ^ "else\n" ^ string_of_c_stmt s2
+  | C_For(e1, e2, e3, s) ->
+      "for (" ^ string_of_c_expr e1  ^ " ; " ^ string_of_c_expr e2 ^ " ; " ^
+      string_of_c_expr e3  ^ ") " ^ string_of_c_stmt s
+  | C_While(e, s) -> "while (" ^ string_of_c_expr e ^ ") " ^ string_of_c_stmt s
+  | C_Stitch(e1,e2,e3,e4,s) ->
+      "stitch " ^ string_of_c_expr e1 ^ " from " ^ string_of_c_expr e2 ^ " to " ^
+        string_of_c_expr e3 ^ " by " ^ string_of_c_expr e4 ^ " : " ^ string_of_c_stmt s
+  | C_Assign(v, e) -> string_of_c_vdecl v ^ " = " ^ string_of_c_expr e ^ ";\n"
+(*   | ArrayDecl(a) -> string_of_c_arraydecl a ^ ";\n" *)
+  (*| ArrayAssign(arraydecl, el) -> "arraydecl;\n" *)
+  | Break -> "break;"
+
+let string_of_c_fdecl fdecl =
+  string_of_c_dataType fdecl.fdecl_type ^ " " ^ fdecl.fdecl_name ^ "(" ^ String.concat ", " (List.map string_of_c_vdecl fdecl.fdecl_formals) ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_c_stmt fdecl.body) ^
+  "}\n"
+
+let string_of_c_program (stmts, funcs) =
+  String.concat "" (List.map string_of_c_stmt stmts) ^ "\n" ^
+  String.concat "\n" (List.map string_of_c_fdecl funcs)
--- ocaml/stch_compiler.ml
+++ ocaml/stch_compiler.ml
@@ -1,13 +1,9 @@
 (* Stitch Compiler *)
 
-(* open Stch_ast
-open Stch_semantic
-open Stch_cast
-
 let in_channel = open_in Sys.argv.(1) in
 
 let lexbuf = Lexing.from_channel in_channel in
 
 let program = Stch_parser.program Stch_scanner.token lexbuf in
-let finalcast =  Stch.semantic.check_program program in
-	print_string "Compiler makes it to the end!\n" *)
+let finalcast =  Stch_semantic.check_prog program in
+	print_string "Compiler makes it to the end!\n"
