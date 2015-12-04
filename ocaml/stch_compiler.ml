cdb0b1ac080b6b897e0c89f335f18195c543576b
Tim Waterman <watermantium@gmail.com>
2015-12-03 19:36:34 -0500

    Makefile and compiler edit

--- ocaml/stch_compiler.ml
+++ ocaml/stch_compiler.ml
@@ -1,3 +1,13 @@
 (* Stitch Compiler *)
 
-open Stch_ast
+(* open Stch_ast
+open Stch_semantic
+open Stch_cast
+
+let in_channel = open_in Sys.argv.(1) in
+
+let lexbuf = Lexing.from_channel in_channel in
+
+let program = Stch_parser.program Stch_scanner.token lexbuf in
+let finalcast =  Stch.semantic.check_program program in
+	print_string "Compiler makes it to the end!\n" *)
